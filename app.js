0a83e343159285ed323f3040ef576795533c5f7a
FrancoCasanova1 <franco123casanova123@gmail.com>
2021-11-03 01:58:29 -0300

    ejs ProductDetail

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const express = require('express')
-const app = express()
-const path = require("path")
-const PUERTO = 3000
-app.use(express.static('./public'));
-    
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get('/', function(req,res){res.sendFile(path.join(__dirname, './views/index.html'))});
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var productDetailRouter = require('./routes/productDetail');
 
-app.get('/register',(req,res)=>{
-    res.sendFile(path.join(__dirname, './views/register.html'))
-})
-app.get('/login',(req,res)=>{
-    res.sendFile(path.join(__dirname, './views/login.html'))
-    
-})
-app.get('/carrito',(req,res)=>{
-    res.sendFile(path.join(__dirname, './views/carrito.html'))
-})
-app.get('/productDetail',(req,res)=>{
-    res.sendFile(path.join(__dirname, './views/productDetail.html'))
-})    
+var app = express();
 
-app.listen(PUERTO, () => console.log('Servidor corriendo; PUERTO 3000'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/productDetail', productDetailRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
