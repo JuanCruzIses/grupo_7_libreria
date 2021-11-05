2e3f790501dddf8e41e93fe3f4e67936144dd7fa
Marcos ISES <juancruzises@gmail.com>
2021-11-05 12:47:55 -0300

    Estilo de footer creado y ya visible en todas las vistas

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
-const biblioteca = fs.readFileSync('../data/libros.json', 'utf-8')
+const biblioteca = fs.readFileSync('./data/libros.json', 'utf-8')
 const libros = JSON.parse(biblioteca);
+
+const novedades = libros.filter(libro => libro.seccion === "novedades");
+const masVendidos = libros.filter(libro => libro.seccion === "masVendidos");
 
 const mainController = {
     index : (req, res) => {
-        res.render('index', {libros})
+        res.render('index', {libros, novedades, masVendidos})
     }
 };
 
