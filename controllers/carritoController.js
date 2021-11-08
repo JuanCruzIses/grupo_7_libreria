9927531f63ba41e04fb17dc5d7cc5477d9ddbfb0
Ro88900721 <90660522+Ro88900721@users.noreply.github.com>
2021-11-07 23:12:25 -0300

    carrito editando

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const biblioteca = fs.readFileSync('./data/libros.json', 'utf-8')
+const libros = JSON.parse(biblioteca);
+
+const productDetailController = {
+    productDetail: (req, res) => {
+        res.render('productDetail', { libros, title: 'Detalle de Producto', id: (req.params.id - 1)})
+    }
+}
 const carritoController = {
     carrito : (req, res) => {
         res.render('carrito')
