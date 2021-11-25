934cc373929ddb494b5775d4398769917ce1ccba
Marcos ISES <juancruzises@gmail.com>
2021-11-24 22:40:15 -0300

    Ajustes en el registerController.registrar (valida que no se repita un usuario) + Metodo adminController.destroy completamente funcional

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
 
 const usersFilePath = path.join(__dirname, '../data/users.json');
 const listaUsuarios = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -14,26 +15,31 @@ const registerController = {
         let ultimoId = Number(listaUsuarios[listaUsuarios.length -1].id);
 		let nuevoUltimoId = (ultimoId + 1).toString();
         let errores = validationResult(req)
+        let usuarioEnBD = listaUsuarios.filter(usuario => usuario.email == req.body.email)
+        
+        if (usuarioEnBD){
+            return res.render('register', {errores: [{msg: 'Este email ya se encuentra registrado'}] })
+        }
 
         if (req.body.contraseña == req.body.confirmaContraseña && errores.isEmpty() ) {
             const nuevoUsuario = {
                 nombre: req.body.nombre,
                 apellido: req.body.apellido,
-                email: req.body.email,
+                email: bcrypt.hashSync(req.body.email, 10),
                 contraseña: req.body.contraseña,
                 confirmaContraseña: req.body.confirmaContraseña,
                 id: nuevoUltimoId,
                 categoria:"users",
             }
             listaUsuarios.push(nuevoUsuario);
-            fs.writeFileSync(usersFilePath, JSON.stringify(listaUsuarios, null))
+            fs.writeFileSync(usersFilePath, JSON.stringify(listaUsuarios, null, ' '))
             res.redirect('/login')
         }
         else {
             return res.render ("register", { errores : errores.array(), old : req.body })
         } 
          
-        }
+        } 
 };
 
 module.exports = registerController;
