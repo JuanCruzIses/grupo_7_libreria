32b9ebc89ddef1f7917b538c26c5102224fa33e2
JuanCruzIses <juancruzises@gmail.com>
2023-02-24 13:38:49 -0300

    .

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const app = require('./src/app')
+// const app = require('./src/app')
 
-
+import express from 'express'
+const app = express()
 const port = process.env.PORT || 3000
+
+app.use("/", (req, res)=>{
+    res.json({message: "Esta andando esto"})
+})
 
 app.listen(port)
 console.log(`Listen on PORT ${port}`)
